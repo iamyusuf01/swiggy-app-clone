7b78aa207b964bf63926ce7f988d5d3255ec4a40
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2024-08-07 21:33:51 +0530

    update

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="bg bg-red-500">
+            <div className="bg-black">
+            <h1>Welcome to the Blog!</h1>
+            <p>This is a simple blog created with React.</p>
+            </div>
+        </header>
+    );
+};
+
+
+export default Header;
